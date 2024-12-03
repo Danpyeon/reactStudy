3c9e56b98722c9e11c484eaeb2444e38351ff7b3
KJH <joohyeok.kim@gmail.com>
2024-12-03 13:42:13 +0900

    Feat: 외부 component 불러오기

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
+import Calc1 from './comp/calc/study01'
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <About />
         <Routes>
           <Route path={"/"} element={<Home/>} />
           <Route path={"/about"} element={<About />} />
+          <Route path={"/calc1"} element={<Calc1 />} />
         </Routes>
       </BrowserRouter>
     </div>
@@ -19,15 +23,15 @@ function Home() {
   return(
     <div>
       <h1>Start Home</h1>
-      <Link to="/about">Move to About</Link>
+      <Link to="/about">Move to About</Link> <br/>
+      <Link to="/calc1">Move to Calc1</Link>
     </div>
   )
 }
 
 function About() {
   return(
-    <div>
-      <h1>About</h1>
+    <div style={{border: '2px solid black'}}>
       <Link to="/">Move to Home</Link>
     </div>
   )
